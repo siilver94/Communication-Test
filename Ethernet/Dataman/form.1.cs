857579eeb7db5fc577d82e185f002a3ab6b2fb2d
Eun Sung Allen Kim <siiilver9@gmail.com>
2022-07-05 09:39:13 +0900

    Create form.1.cs
    Code for Cognex Dataman Ethernet Communication.

---
+++ Ethernet/Dataman/form.1.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.Text;
+using Ken2.Util;
+using System.Diagnostics;
+using System.IO;
+
+namespace EthernetTest
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tc;
+        public TCPClient_PLC1 tck;
+
+        string msg = "";
+
+        NetworkStream stream;
+
+
+
+        private delegate void dele(); // delegate
+
+
+        private static DateTime Delay(int MS)
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = DateTime.Now;
+            }
+            return DateTime.Now;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //fffffffffff
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)   //접속
+        {
+            // (1) IP 주소와 포트를 지정하고 TCP 연결 
+            tc = new TcpClient(textBox3.Text, Convert.ToInt32(textBox1.Text));
+            tck = new TCPClient_PLC1(textBox3.Text, Convert.ToInt32(textBox1.Text), 100, this);
+            // tck.TalkingComm += plc1_TalkingComm;
+            //TcpClient tc = new TcpClient("localhost", 7000);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)  // 종료
+        {
+            tc.Dispose();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)  //전송
+        {
+
+            //msg = textBox2.Text;
+            //byte[] buff = Encoding.ASCII.GetBytes(msg);
+
+            //// (2) NetworkStream을 얻어옴 
+            //stream = tc.GetStream();
+            //stream.Write(buff, 0, buff.Length);
+
+            tck.MCWrite(Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox5.Text));
+
+            richTextBox1.AppendText(Dtime.Now(Dtime.StringType.CurrentTime) + " : " + textBox5.Text + Environment.NewLine);
+            richTextBox1.ScrollToCaret();
+            Delay(100);
+        }
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //cccccccccccc
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            tc.Dispose();
+        }
+    }
+
+}
